d69784d00fbbad53acef093782ac45e3e73f7833
Andreas Rayo Kniep <zeug@rayo.de>
2014-11-19 18:15:34 -0800

    (metadata)  fixed bugs and added version

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,9 @@ maintainer        "CrowdMob Inc."
 maintainer_email  "developers@crowdmob.com"
 license           "Apache 2.0"
 description       "SCM checkout, bundle install, and restart Sinatra web applications (relies on aws-opsworks/deploy)."
+version           "0.1"
+
 depends           "deploy"
-recipe            "sinatra::configure"
-recipe            "sinatra::deploy"
+recipe            "sinatra::configure", "recipe to create all deploy(ment) folder(s)"
+recipe            "sinatra::deploy", "recipe to deploy Sinatra application"
+
